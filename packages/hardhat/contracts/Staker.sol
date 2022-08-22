83edfe7bad4d8a8f2c5f64d568b8d32fed011832
ERIC858 <eric858826@gmail.com>
2022-08-22 03:57:23 +0000

    编写质押合约

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,35 +1,257 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.4;
 
+
+
 import "hardhat/console.sol";
+
 import "./ExampleExternalContract.sol";
+
+
 
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+
+
+  constructor(address exampleExternalContractAddress) public {
+
+      exampleExternalContract = ExampleExternalContract(
+
+          exampleExternalContractAddress
+
+      );
+
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+  //Solidity 映射
+
+  //多少 ETH 存入合约
+
+  mapping(address => uint256) public balances;
+
+  //存款发生的时间
+
+  mapping(address => uint256) public depositTimestamps;
 
 
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
+
+  //公共变量
+
+  //利率
+
+  uint256 public constant rewardRatePerSecond = 0.1 ether;
+
+  //质押日期
+
+  uint256 public withdrawalDeadline = block.timestamp + 120 seconds;
+
+  //结束日期
+
+  uint256 public claimDeadline = block.timestamp + 240 seconds;
+
+  //当前区块
+
+  uint256 public currentBlock = 0;
 
 
-  // Add a `withdraw()` function to let users withdraw their balance
+
+  event Stake(address indexed sender, uint256 amount);
+
+  event Received(address, uint256);
+
+  event Execute(address indexed sender, uint256 amount);
 
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+
+/**
+
+  控制提款窗口的开始和结束
+
+ */
+
+  function withdrawalTimeLeft() public view returns (uint256 withdrawalTimeLeft) {
+
+    if( block.timestamp >= withdrawalDeadline) {
+
+      return (0);
+
+    } else {
+
+      return (withdrawalDeadline - block.timestamp);
+
+    }
+
+  }
 
 
-  // Add the `receive()` special function that receives eth and calls stake()
+
+  function claimPeriodLeft() public view returns (uint256 claimPeriodLeft) {
+
+    if( block.timestamp >= claimDeadline) {
+
+      return (0);
+
+    } else {
+
+      return (claimDeadline - block.timestamp);
+
+    }
+
+  }
+
+
+
+  /**
+
+    控制决定我们的质押、提款和遣返功能
+
+  */
+
+  modifier withdrawalDeadlineReached( bool requireReached ) {
+
+    uint256 timeRemaining = withdrawalTimeLeft();
+
+    if( requireReached ) {
+
+      require(timeRemaining == 0, "Withdrawal period is not reached yet");
+
+    } else {
+
+      require(timeRemaining > 0, "Withdrawal period has been reached");
+
+    }
+
+    _;
+
+  }
+
+
+
+  modifier claimDeadlineReached( bool requireReached ) {
+
+    uint256 timeRemaining = claimPeriodLeft();
+
+    if( requireReached ) {
+
+      require(timeRemaining == 0, "Claim deadline is not reached yet");
+
+    } else {
+
+      require(timeRemaining > 0, "Claim deadline has been reached");
+
+    }
+
+    _;
+
+  }
+
+
+
+  modifier notCompleted() {
+
+    bool completed = exampleExternalContract.completed();
+
+    require(!completed, "Stake already completed!");
+
+    _;
+
+  }
+
+
+
+
+
+/**
+
+  充值/质押功能
+
+ */  
+
+  function stake() public payable withdrawalDeadlineReached(false) claimDeadlineReached(false) {
+
+    balances[msg.sender] = balances[msg.sender] + msg.value;
+
+    depositTimestamps[msg.sender] = block.timestamp;
+
+    emit Stake(msg.sender, msg.value);
+
+  }
+
+
+
+  /**
+
+    提现功能
+
+   */
+
+   function withdraw() public withdrawalDeadlineReached(true) claimDeadlineReached(false) notCompleted{
+
+    require(balances[msg.sender] > 0, "You have no balance to withdraw!");
+
+    uint256 individualBalance = balances[msg.sender];
+
+    uint256 indBalanceRewards = individualBalance + ((block.timestamp-depositTimestamps[msg.sender])*rewardRatePerBlock);
+
+    balances[msg.sender] = 0;
+
+
+
+    // Transfer all ETH via call! (not transfer) cc: https://solidity-by-example.org/sending-ether
+
+    (bool sent, bytes memory data) = msg.sender.call{value: indBalanceRewards}("");
+
+    require(sent, "RIP; withdrawal failed :( ");
+
+  }
+
+
+
+  //遣返功能
+
+  function execute() public claimDeadlineReached(true) notCompleted {
+
+    uint256 contractBalance = address(this).balance;
+
+    exampleExternalContract.complete{value: address(this).balance}();
+
+  }
+
+
+
+  /*
+
+  Time to "kill-time" on our local testnet
+
+  */
+
+  function killTime() public {
+
+    currentBlock = block.timestamp;
+
+  }
+
+
+
+  /*
+
+  \Function for our smart contract to receive ETH
+
+  cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
+
+  */
+
+  receive() external payable {
+
+      emit Received(msg.sender, msg.value);
+
+  }
+
 
 
 }
